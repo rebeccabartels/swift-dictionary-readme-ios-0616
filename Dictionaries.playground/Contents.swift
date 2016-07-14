d4c9795b6f580d644af25d37b934aec40ae22440
rebeccabartels <rebeccakbartels@icloud.com>
2016-07-14 17:15:03 -0400

    donezo

--- Dictionaries.playground/Contents.swift
+++ Dictionaries.playground/Contents.swift
@@ -4,6 +4,7 @@ var favoriteWords = [
     "lollygag": "spend time aimlessly",
     "wabbit": "weary, exhausted",
 ]
+//(Note that you don't have to put each item on a separate line. You could have declared this variable in a single line, as long as the items are separated by commas, but putting one item on each line makes it a lot easier to read.)
 
 var numberWords = [1: "one"]
 
@@ -16,15 +17,23 @@ if let definition = definition {
 } else {
     print("'rigmarole' isn't a word!")
 }
+//
 
 if let definition = nonexistentDefinition {
     print("The definition of 'strategery' is '\(definition)'")
 } else {
     print("'strategery' isn't a word")
 }
+//ince favoriteWords is a variable, you can add new items to it. (If you had declared it as a constant using let, however, you would not be able to add items to it, just like you can't add items to a constant array.) It's pretty easy to add "strategery" to your dictionary. Just assign a value to the key:
+//You can see this if you try to unwrap and print the definitions you retrieved from the dictionary:
+
 
 favoriteWords["strategery"] = "strategy"
 definition = favoriteWords["strategery"]
+//You can easily check to see if that key is now a part of your dictionary, either by printing the entire dictionary to the console, or by retrieving the definition using "strategery" as the key:
+
+
 
 favoriteWords["strategery"] = nil
 print(favoriteWords)
+//decide to remove it from the dictionary. Removing a key from a dictionary is pretty easy: Just assign that key the nil value.
